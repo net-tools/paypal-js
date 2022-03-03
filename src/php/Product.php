5913094a0e676f1bc1cbe26dfbc97262619804ed
Pierre <dev@nettools.ovh>
2022-03-03 12:13:06 +0100

    php8

--- src/php/Product.php
+++ src/php/Product.php
@@ -43,9 +43,9 @@ class Product {
 		$this->category = $category;
 		$this->currency_code = $currency_code;
 		
-		$this->tax = isset($other['tax'])?$other['tax']:0;
-		$this->sku = isset($other['sku'])?$other['sku']:'';
-		$this->description = isset($other['description'])?$other['description']:'';
+		$this->tax = !empty($other['tax'])?$other['tax']:0;
+		$this->sku = !empty($other['sku'])?$other['sku']:'';
+		$this->description = !empty($other['description'])?$other['description']:'';
 	}
 	
 }
